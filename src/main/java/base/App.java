baf2ac41edf73bea59bb710b0e1ad23f2210123b
Peachey-Desktop <the1peachey@gmail.com>
2021-06-05 11:14:38 -0400

    finalized base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -8,14 +8,14 @@ package base;
 /*
 Create a program that prompts for your name and prints a greeting using your name.
 
-Example Output
+Example Output:
 What is your name? Brian
 Hello, Brian, nice to meet you!
 
-Constraint
+Constraint:
 Keep the input, string concatenation, and output separate.
 
-Challenges
+Challenges:
 Write a new version of the program without using any variables.
 Write a version of the program that displays different greetings for different people.
 Write a version of the program that displays the greeting in a GUI window.
